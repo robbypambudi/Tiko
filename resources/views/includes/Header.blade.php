afbbbe17420bd68cee6fe0fe49f462b023d396af
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2023-06-06 22:04:36 +0700

    feat: add tentang tiko, artis populer, event saat ini, tiko tips, and footer

--- resources/views/includes/Header.blade.php
+++ resources/views/includes/Header.blade.php
@@ -33,12 +33,12 @@
 </li>
 <!-- </ul> -->
 <li class="mr-3">
-  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;">Profil</a>
+  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;"><img src="{{asset("images/landing/profil.png")}}" /></a>
 </li>
 <!-- </ul> -->
-<li class="mr-3">
+{{-- <li class="mr-3">
   <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;">Notif</a>
-</li>
+</li> --}}
 
       <!-- <button id="navAction"
         class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
