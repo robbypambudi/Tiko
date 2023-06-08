dd79a1410c2fda7cfd25fd50a1cbba9f6e2c9ebd
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-06-08 23:52:27 +0700

    feat: add navbar trasition

--- resources/views/includes/Header.blade.php
+++ resources/views/includes/Header.blade.php
@@ -2,8 +2,7 @@
   <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
     <div class="pl-4 flex items-center">
       <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-        <!--Icon from: http://www.potlabicons.com/ -->
-        <img src="{{asset("images/landing/logo.png")}}" />
+        <img src="{{asset('images/landing/logo.png')}}" />
       </a>
     </div>
     <div class="block lg:hidden pr-4">
@@ -19,35 +18,28 @@
       class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
       id="nav-content">
       <ul class="list-reset lg:flex justify-end flex-1 items-center">
-      <li class="mr-3">
-  <a class="inline-block py-2 px-4 text-white font-bold no-underline" href="#" style="font-family: 'Inter', sans-serif;">Home</a>
-</li>
-<li class="mr-3">
-  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;">Artis</a>
-</li>
-<li class="mr-3">
-  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;">Event</a>
-</li>
-<li class="mr-3">
-  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;">Tiko Tips</a>
-</li>
-<!-- </ul> -->
-<li class="mr-3">
-  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;"><img src="{{asset("images/landing/profil.png")}}" /></a>
-</li>
-<!-- </ul> -->
-{{-- <li class="mr-3">
-  <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#" style="font-family: 'Inter', sans-serif;">Notif</a>
-</li> --}}
-
-      <!-- <button id="navAction"
-        class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-        User
-      </button>
-      <button id="navAction"
-        class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-        Notif
-      </button> -->
+        <li class="mr-3">
+          <a class="inline-block py-2 px-4 text-white font-bold no-underline" href="#"
+            style="font-family: 'Inter', sans-serif;">Home</a>
+        </li>
+        <li class="mr-3">
+          <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#"
+            style="font-family: 'Inter', sans-serif;">Artis</a>
+        </li>
+        <li class="mr-3">
+          <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#"
+            style="font-family: 'Inter', sans-serif;">Event</a>
+        </li>
+        <li class="mr-3">
+          <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#"
+            style="font-family: 'Inter', sans-serif;">Tiko Tips</a>
+        </li>
+        <!-- </ul> -->
+        <li class="mr-3">
+          <a class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#"
+            style="font-family: 'Inter', sans-serif;"><img src="{{asset('images/landing/profil.png')}}" /></a>
+        </li>
+      </ul>
     </div>
   </div>
   <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
